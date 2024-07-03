a86237c90f4ab0505235b29fa9a4efa62ebb8047
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-07-04 01:01:35 +0600

    finish local todo list

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -2,29 +2,42 @@ import { LightningElement } from 'lwc';
 
 export default class Todo extends LightningElement {
     todoValue=''; 
-    todoList=[
-        {
-            id:1,
-            value: 'test',
-            status: true
-        }
-    ];
+    todoList=[];
+
+    idCounter = 0;
+
+    get hasTodoListItems(){
+        return this.todoList.length > 0;
+    }
 
     handleChange(event){
         this.todoValue = event.target.value;
     }
 
+    handleClear(event){
+        this.todoList = [];
+    }
+
     addTodo(event){
-        console.log('addTodo', this.todoValue);
+        if(this.todoValue === ''){
+            alert("You must write something to add something!");
+            return;
+        }
+        this.idCounter++;
         let newTodo = {
-            id: this.todoList.length+1,
+            id: this.idCounter+1,
             value: this.todoValue
         }
         this.todoList = [...this.todoList, newTodo];
+        this.todoValue = '';
     }
 
     deleteHandler(event){
         console.log('deleteHandler', event.detail);
         this.todoList = this.todoList.filter(todo => todo.id !== event.detail);
+    }
+
+    connectedCallback(){
+        this.idCounter = this.todoList.length;
     }
 }
